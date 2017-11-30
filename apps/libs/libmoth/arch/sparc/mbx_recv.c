fb8226cb1da074aaaf8e1c1e38ab26f4b2300dc3
Jean-Christophe Dubois <jcd@tribudubois.net>
2017-11-30 23:28:55 +0100

    Fix MBX related system calls
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- apps/libs/libmoth/arch/sparc/mbx_recv.c
+++ apps/libs/libmoth/arch/sparc/mbx_recv.c
@@ -23,7 +23,9 @@
 
 #include <moth.h>
 
-os_status_t mbx_recv(os_task_id_t *src_id, os_mbx_msg_t *msg) {
+__attribute__((section(".data.entry"))) static os_mbx_entry_t __os_task_mbx_entry;
+
+os_status_t mbx_recv(os_task_id_t *sender_id, os_mbx_msg_t *msg) {
   os_status_t status;
 
   asm volatile("ta 0x03\n"
@@ -31,6 +33,9 @@ os_status_t mbx_recv(os_task_id_t *src_id, os_mbx_msg_t *msg) {
                : "=r"(status)
                :
                : "memory");
+
+  *sender_id = __os_task_mbx_entry.sender_id;
+  *msg = __os_task_mbx_entry.msg;
 
   return status;
 }
