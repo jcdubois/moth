d5d9a3cb7df49f61483749e8b40f45b0e996532d
Jean-Christophe Dubois <jcd@tribudubois.net>
2017-12-07 10:54:53 +0100

    Rework the way we are mappin UART to avoid errors in coverity
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- apps/app3/main.c
+++ apps/app3/main.c
@@ -50,7 +50,7 @@
 #define UART_STATUS_THE 0x00000004
 #define UART_CTRL_TE 0x00000002
 
-extern uint8_t __uart_begin;
+extern uint8_t __uart_begin[UART1_DEVICE_OFFSET + UART_CTRL_REG_OFFSET];
 
 __attribute__((section(".text.entry"))) void entry(uint32_t task_id,
                                                    uint32_t arg2);
@@ -67,7 +67,7 @@ static void putc(void *opaque, char car) {
 }
 
 void entry(uint32_t task_id, uint32_t arg2) {
-  uint32_t uart_addr = (uint32_t)(&__uart_begin + UART1_DEVICE_OFFSET);
+  uint32_t uart_addr = (uint32_t)(&__uart_begin[UART1_DEVICE_OFFSET]);
   os_status_t cr;
   os_task_id_t tmp_id;
   os_mbx_msg_t msg;
