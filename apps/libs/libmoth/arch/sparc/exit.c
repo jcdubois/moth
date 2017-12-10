ec3eb87488e7eefb6a38cba3e85a32db6d1b6388
Jean-Christophe Dubois <jcd@tribudubois.net>
2017-12-10 17:42:31 +0100

    Fix header in exit.c file
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- apps/libs/libmoth/arch/sparc/exit.c
+++ apps/libs/libmoth/arch/sparc/exit.c
@@ -16,9 +16,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * @file yield.c
+ * @file exit.c
  * @author Jean-Christophe Dubois (jcd@tribudubois.net)
- * @brief yield system call
+ * @brief exit system call
  */
 
 #include <moth.h>
