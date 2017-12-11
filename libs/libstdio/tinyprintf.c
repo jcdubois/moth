584ec0370e77fe75e9927b6c30754198ed8d6946
Jean-Christophe Dubois <jcd@tribudubois.net>
2017-12-11 20:32:14 +0100

    Don't use any standard include.
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- libs/libstdio/tinyprintf.c
+++ libs/libstdio/tinyprintf.c
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * Configuration adjustments
  */
 #ifdef PRINTF_SIZE_T_SUPPORT
-#include <sys/types.h>
+#include <types.h>
 #endif
 
 #ifdef PRINTF_LONG_LONG_SUPPORT
