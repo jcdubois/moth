37838cdf688d937c72a183ead033a201eae0adff
Jean-Christophe Dubois <jcd@tribudubois.net>
2019-02-09 15:40:47 +0100

    Shorten state variable name for mailbox
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- kernel/core/moth-mailbox.adb
+++ kernel/core/moth-mailbox.adb
@@ -30,7 +30,7 @@ with Moth.Scheduler;
 
 package body Moth.Mailbox with
    SPARK_mode => On,
-   Refined_State => (State => (os_task_list_mbx_fifo))
+   Refined_State => (State => (mbx_fifo))
 is
 
    -----------------
@@ -64,7 +64,7 @@ is
    -- Private variables --
    -----------------------
 
-   os_task_list_mbx_fifo : array (os_task_id_param_t) of os_mbx_t;
+   mbx_fifo : array (os_task_id_param_t) of os_mbx_t;
 
    ----------------------------------
    -- Private functions/procedures --
@@ -85,7 +85,7 @@ is
    --  Check if the mbx fifo of a given task is empty.
 
    function mbx_is_empty (task_id : os_task_id_param_t) return Boolean
-   is (os_task_list_mbx_fifo (task_id).count = os_mbx_count_t'First);
+   is (mbx_fifo (task_id).count = os_mbx_count_t'First);
 
    --------------------
    -- mbx_is_full --
@@ -93,7 +93,7 @@ is
    --  Check if the mbx fifo of a given task is full.
 
    function mbx_is_full (task_id : os_task_id_param_t) return Boolean
-   is (os_task_list_mbx_fifo (task_id).count = os_mbx_count_t'Last);
+   is (mbx_fifo (task_id).count = os_mbx_count_t'Last);
 
    -------------------------
    -- get_mbx_head --
@@ -102,7 +102,7 @@ is
 
    function get_mbx_head
      (task_id : os_task_id_param_t) return os_mbx_index_t
-   is (os_task_list_mbx_fifo (task_id).head);
+   is (mbx_fifo (task_id).head);
 
    ------------------
    -- inc_mbx_head --
@@ -113,7 +113,7 @@ is
    procedure inc_mbx_head (task_id : os_task_id_param_t)
    is
    begin
-      os_task_list_mbx_fifo (task_id).head :=
+      mbx_fifo (task_id).head :=
               os_mbx_index_t'Succ (get_mbx_head (task_id));
    end inc_mbx_head;
 
@@ -124,7 +124,7 @@ is
 
    function get_mbx_count
      (task_id : os_task_id_param_t) return os_mbx_count_t
-   is (os_task_list_mbx_fifo (task_id).count);
+   is (mbx_fifo (task_id).count);
 
    ------------------
    -- get_mbx_tail --
@@ -136,7 +136,7 @@ is
    is (get_mbx_head (task_id) +
        os_mbx_count_t'Pred (get_mbx_count (task_id)))
    with
-      Global => (Input => os_task_list_mbx_fifo),
+      Global => (Input => mbx_fifo),
       Pre => not mbx_is_empty (task_id);
 
    -------------------
@@ -146,12 +146,12 @@ is
 
    procedure inc_mbx_count (task_id : os_task_id_param_t)
    with
-      Global => (In_Out => os_task_list_mbx_fifo),
-      Pre => (os_task_list_mbx_fifo (task_id).count < os_mbx_count_t'Last),
-      Post => os_task_list_mbx_fifo = os_task_list_mbx_fifo'Old'Update (task_id => os_task_list_mbx_fifo'Old (task_id)'Update (count => os_task_list_mbx_fifo'Old (task_id).count + 1))
+      Global => (In_Out => mbx_fifo),
+      Pre => (mbx_fifo (task_id).count < os_mbx_count_t'Last),
+      Post => mbx_fifo = mbx_fifo'Old'Update (task_id => mbx_fifo'Old (task_id)'Update (count => mbx_fifo'Old (task_id).count + 1))
    is
    begin
-      os_task_list_mbx_fifo (task_id).count :=
+      mbx_fifo (task_id).count :=
               os_mbx_count_t'Succ (get_mbx_count (task_id));
    end inc_mbx_count;
 
@@ -162,12 +162,12 @@ is
 
    procedure dec_mbx_count (task_id : os_task_id_param_t)
    with
-      Global => (In_Out => os_task_list_mbx_fifo),
+      Global => (In_Out => mbx_fifo),
       Pre => (not mbx_is_empty (task_id)),
-      Post => os_task_list_mbx_fifo = os_task_list_mbx_fifo'Old'Update (task_id => os_task_list_mbx_fifo'Old (task_id)'Update (count => os_task_list_mbx_fifo'Old (task_id).count - 1))
+      Post => mbx_fifo = mbx_fifo'Old'Update (task_id => mbx_fifo'Old (task_id)'Update (count => mbx_fifo'Old (task_id).count - 1))
    is
    begin
-      os_task_list_mbx_fifo (task_id).count :=
+      mbx_fifo (task_id).count :=
               os_mbx_count_t'Pred (get_mbx_count (task_id));
    end dec_mbx_count;
 
@@ -181,19 +181,19 @@ is
       src_id  : os_task_id_param_t;
       mbx_msg : os_mbx_msg_t)
    with
-      Global => (In_Out => os_task_list_mbx_fifo),
+      Global => (In_Out => mbx_fifo),
       Pre  => ((not mbx_is_full (dest_id)) and then
                os_ghost_mbx_are_well_formed),
       Post => ((not mbx_is_empty (dest_id)) and then
-               (os_task_list_mbx_fifo = os_task_list_mbx_fifo'Old'Update (dest_id => os_task_list_mbx_fifo'Old (dest_id)'Update (count => os_task_list_mbx_fifo'Old (dest_id).count + 1, head => os_task_list_mbx_fifo'Old (dest_id).head, mbx_array => os_task_list_mbx_fifo'Old (dest_id).mbx_array'Update (get_mbx_tail (dest_id) => os_task_list_mbx_fifo'Old (dest_id).mbx_array (get_mbx_tail (dest_id))'Update (sender_id => src_id, msg => mbx_msg))))) and then
+               (mbx_fifo = mbx_fifo'Old'Update (dest_id => mbx_fifo'Old (dest_id)'Update (count => mbx_fifo'Old (dest_id).count + 1, head => mbx_fifo'Old (dest_id).head, mbx_array => mbx_fifo'Old (dest_id).mbx_array'Update (get_mbx_tail (dest_id) => mbx_fifo'Old (dest_id).mbx_array (get_mbx_tail (dest_id))'Update (sender_id => src_id, msg => mbx_msg))))) and then
                os_ghost_mbx_are_well_formed)
    is
       mbx_index : os_mbx_index_t;
    begin
       inc_mbx_count (dest_id);
       mbx_index := get_mbx_tail (dest_id);
-      os_task_list_mbx_fifo (dest_id).mbx_array (mbx_index).sender_id := src_id;
-      os_task_list_mbx_fifo (dest_id).mbx_array (mbx_index).msg := mbx_msg;
+      mbx_fifo (dest_id).mbx_array (mbx_index).sender_id := src_id;
+      mbx_fifo (dest_id).mbx_array (mbx_index).msg := mbx_msg;
    end mbx_add_message;
 
    --------------------------
@@ -203,11 +203,11 @@ is
    function get_mbx_entry_sender
      (task_id   : os_task_id_param_t;
       index : os_mbx_count_t) return os_task_id_param_t
-   is (os_task_id_param_t (os_task_list_mbx_fifo (task_id).mbx_array
+   is (os_task_id_param_t (mbx_fifo (task_id).mbx_array
            (get_mbx_head (task_id) + index).sender_id))
    with
-      Global => (Input => (os_task_list_mbx_fifo)),
-      Pre => os_task_list_mbx_fifo (task_id).mbx_array
+      Global => (Input => (mbx_fifo)),
+      Pre => mbx_fifo (task_id).mbx_array
                   (get_mbx_head (task_id) + index).sender_id
                   in os_task_id_param_t;
 
@@ -254,7 +254,7 @@ is
                             -- os_task_list_next,
                             -- os_task_list_prev,
                             -- os_ghost_task_list_ready,
-                            os_task_list_mbx_fifo),
+                            mbx_fifo),
                  Input  => (Moth.Config.State,
                             Moth.Current.State,
                             os_task_list_mbx_mask)),
@@ -303,7 +303,7 @@ is
                             -- os_task_list_next,
                             -- os_task_list_prev,
                             -- os_ghost_task_list_ready,
-                            os_task_list_mbx_fifo),
+                            mbx_fifo),
                  Input  => (Moth.Config.State,
                             Moth.Current.State,
                             os_task_list_mbx_mask)),
@@ -343,9 +343,9 @@ is
       mbx_index : os_mbx_index_t)
    is
    begin
-      os_task_list_mbx_fifo (task_id).mbx_array (mbx_index).sender_id :=
+      mbx_fifo (task_id).mbx_array (mbx_index).sender_id :=
                                                                OS_TASK_ID_NONE;
-      os_task_list_mbx_fifo (task_id).mbx_array (mbx_index).msg := 0;
+      mbx_fifo (task_id).mbx_array (mbx_index).msg := 0;
    end clear_mbx_entry;
 
    -------------------
@@ -359,7 +359,7 @@ is
       mbx_entry : os_mbx_entry_t)
    is
    begin
-      os_task_list_mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index) := mbx_entry;
+      mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index) := mbx_entry;
    end set_mbx_entry;
 
    -------------------
@@ -369,7 +369,7 @@ is
    function get_mbx_entry
      (task_id   : os_task_id_param_t;
       index : os_mbx_count_t) return os_mbx_entry_t
-   is (os_task_list_mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index));
+   is (mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index));
 
    --------------------------
    -- is_waiting_mbx_entry --
@@ -383,7 +383,7 @@ is
                 (Unsigned_32'(1), Natural (get_mbx_entry_sender
                 (task_id, index))))) /= 0)
    with
-      Global => (Input => (os_task_list_mbx_mask, os_task_list_mbx_fifo)),
+      Global => (Input => (os_task_list_mbx_mask, mbx_fifo)),
       Pre => not mbx_is_empty (task_id) and then
              os_ghost_mbx_are_well_formed and then
              index < get_mbx_count (task_id) and then
@@ -412,14 +412,14 @@ is
    function os_ghost_task_mbx_are_well_formed (task_id : os_task_id_param_t) return Boolean is
       (for all index in os_mbx_index_t'Range =>
          (if (os_mbx_count_t(index) >= get_mbx_count (task_id))
-          then (os_task_list_mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index).sender_id = OS_TASK_ID_NONE)
-          else (os_task_list_mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index).sender_id in os_task_id_param_t)))
+          then (mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index).sender_id = OS_TASK_ID_NONE)
+          else (mbx_fifo (task_id).mbx_array (get_mbx_head (task_id) + index).sender_id in os_task_id_param_t)))
    with
       Ghost => true,
       Post => os_ghost_task_mbx_are_well_formed'Result =
          (for all index in os_mbx_index_t'Range =>
             (if (mbx_is_empty (task_id))
-             then (os_task_list_mbx_fifo (task_id).mbx_array (index).sender_id
+             then (mbx_fifo (task_id).mbx_array (index).sender_id
                      = OS_TASK_ID_NONE)
              else (if (((get_mbx_tail (task_id)
                             < get_mbx_head (task_id)) and
@@ -427,9 +427,9 @@ is
                          (index <= get_mbx_tail (task_id)))) or else
                        (index in get_mbx_head (task_id) ..
                                  get_mbx_tail (task_id)))
-                   then (os_task_list_mbx_fifo (task_id).mbx_array (index).sender_id
+                   then (mbx_fifo (task_id).mbx_array (index).sender_id
                            in os_task_id_param_t)
-                   else (os_task_list_mbx_fifo (task_id).mbx_array (index).sender_id
+                   else (mbx_fifo (task_id).mbx_array (index).sender_id
                            = OS_TASK_ID_NONE))));
 
       pragma Annotate (GNATprove, Inline_For_Proof,
@@ -454,9 +454,9 @@ is
    procedure remove_first_mbx
       (task_id    : in os_task_id_param_t)
    with
-      Global => (In_Out => os_task_list_mbx_fifo),
+      Global => (In_Out => mbx_fifo),
       Pre  => (not mbx_is_empty (task_id)) and os_ghost_mbx_are_well_formed,
-      Post => (os_task_list_mbx_fifo = os_task_list_mbx_fifo'Old'Update (task_id => os_task_list_mbx_fifo'Old (task_id)'Update (count => os_mbx_count_t'Pred (os_task_list_mbx_fifo'Old (task_id).count), head => os_mbx_index_t'Succ (os_task_list_mbx_fifo'Old (task_id).head), mbx_array => os_task_list_mbx_fifo'Old (task_id).mbx_array'Update (os_mbx_index_t'Pred (get_mbx_head (task_id)) => (sender_id => OS_TASK_ID_NONE, msg => 0)))))
+      Post => (mbx_fifo = mbx_fifo'Old'Update (task_id => mbx_fifo'Old (task_id)'Update (count => os_mbx_count_t'Pred (mbx_fifo'Old (task_id).count), head => os_mbx_index_t'Succ (mbx_fifo'Old (task_id).head), mbx_array => mbx_fifo'Old (task_id).mbx_array'Update (os_mbx_index_t'Pred (get_mbx_head (task_id)) => (sender_id => OS_TASK_ID_NONE, msg => 0)))))
               and os_ghost_mbx_are_well_formed
    is
       mbx_index   : constant os_mbx_index_t := get_mbx_head (task_id);
@@ -479,9 +479,9 @@ is
    procedure remove_last_mbx
       (task_id    : in os_task_id_param_t)
    with
-      Global => (In_Out => os_task_list_mbx_fifo),
+      Global => (In_Out => mbx_fifo),
       Pre  => (not mbx_is_empty (task_id)) and os_ghost_mbx_are_well_formed,
-      Post => (os_task_list_mbx_fifo = os_task_list_mbx_fifo'Old'Update (task_id => os_task_list_mbx_fifo'Old (task_id)'Update (count => os_mbx_count_t'Pred (os_task_list_mbx_fifo'Old (task_id).count), head => os_task_list_mbx_fifo'Old (task_id).head, mbx_array => os_task_list_mbx_fifo'Old (task_id).mbx_array'Update (os_task_list_mbx_fifo (task_id).head + os_task_list_mbx_fifo (task_id).count => (sender_id => OS_TASK_ID_NONE, msg => 0)))))
+      Post => (mbx_fifo = mbx_fifo'Old'Update (task_id => mbx_fifo'Old (task_id)'Update (count => os_mbx_count_t'Pred (mbx_fifo'Old (task_id).count), head => mbx_fifo'Old (task_id).head, mbx_array => mbx_fifo'Old (task_id).mbx_array'Update (mbx_fifo (task_id).head + mbx_fifo (task_id).count => (sender_id => OS_TASK_ID_NONE, msg => 0)))))
               and os_ghost_mbx_are_well_formed
    is
       mbx_index   : constant os_mbx_index_t := get_mbx_tail (task_id);
@@ -502,14 +502,14 @@ is
       (task_id    : in os_task_id_param_t;
        index      : in os_mbx_count_t)
    with
-      Global => (In_Out => os_task_list_mbx_fifo),
+      Global => (In_Out => mbx_fifo),
       Pre => (not mbx_is_empty (task_id)) and
              os_ghost_mbx_are_well_formed and
              (index > 0) and
              (index < os_mbx_count_t'Pred (get_mbx_count (task_id))),
       Post => os_ghost_mbx_are_well_formed and
-              os_task_list_mbx_fifo (task_id).count = os_task_list_mbx_fifo'Old (task_id).count and
-              os_task_list_mbx_fifo (task_id).head = os_task_list_mbx_fifo'Old (task_id).head
+              mbx_fifo (task_id).count = mbx_fifo'Old (task_id).count and
+              mbx_fifo (task_id).head = mbx_fifo'Old (task_id).head
    is begin
       for iterator in index ..
                       os_mbx_count_t'Pred (get_mbx_count (task_id)) loop
@@ -555,8 +555,8 @@ is
             -- unconditionnaly in all cases. This loop invariant allows the
             -- prover to work but it should be removed later when the prover
             -- supports branches with exit path.
-            pragma Loop_Invariant (os_task_list_mbx_fifo =
-                                         os_task_list_mbx_fifo'Loop_Entry);
+            pragma Loop_Invariant (mbx_fifo =
+                                         mbx_fifo'Loop_Entry);
 
             --  is this a mbx we are waiting for
             if is_waiting_mbx_entry (current, iterator) then
@@ -631,13 +631,13 @@ is
       for task_iterator in os_task_id_param_t'Range loop
 
          for mbx_iterator in os_mbx_index_t'Range loop
-            os_task_list_mbx_fifo (task_iterator).mbx_array (mbx_iterator).sender_id := OS_TASK_ID_NONE;
-            os_task_list_mbx_fifo (task_iterator).mbx_array (mbx_iterator).msg := 0;
+            mbx_fifo (task_iterator).mbx_array (mbx_iterator).sender_id := OS_TASK_ID_NONE;
+            mbx_fifo (task_iterator).mbx_array (mbx_iterator).msg := 0;
             -- clear_mbx_entry (task_iterator, mbx_iterator);
          end loop;
 
-         os_task_list_mbx_fifo (task_iterator).head := os_mbx_index_t'First;
-         os_task_list_mbx_fifo (task_iterator).count := os_mbx_count_t'First;
+         mbx_fifo (task_iterator).head := os_mbx_index_t'First;
+         mbx_fifo (task_iterator).count := os_mbx_count_t'First;
 
          os_task_list_mbx_mask (task_iterator) := 0;
 
