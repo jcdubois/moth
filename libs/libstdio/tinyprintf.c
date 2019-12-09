97ecadfd7e182bdc2f792bff722397bef0af1742
Jean-Christophe Dubois <jcd@tribudubois.net>
2019-12-09 21:02:05 +0100

    Remove support for long int support.
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- libs/libstdio/tinyprintf.c
+++ libs/libstdio/tinyprintf.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 /* Enable long int support */
-#define PRINTF_LONG_SUPPORT
+//#define PRINTF_LONG_SUPPORT
 
 /* Enable long long int support (implies long int support) */
 //#define PRINTF_LONG_LONG_SUPPORT
