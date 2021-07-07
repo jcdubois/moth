55fcf015e25dbe2c7ada7efb95652934312756f9
Jean-Christophe Dubois <jcd@tribudubois.net>
2021-07-07 22:56:33 +0200

    Get more direct call to OS service from trap handler.
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- kernel/arch/sparc/cpu/common/os_arch_sparc_syscalls.c
+++ kernel/arch/sparc/cpu/common/os_arch_sparc_syscalls.c
@@ -47,7 +47,7 @@ uint32_t *os_arch_init(void) {
 /**
  * Wait function handler.
  */
-static uint32_t *os_arch_sched_wait(uint32_t *ctx) {
+uint32_t *os_arch_sched_wait(uint32_t *ctx) {
   os_task_id_t current_task_id;
   os_task_id_t new_task_id;
   os_mbx_mask_t mbx_mask = (os_mbx_mask_t)(*(ctx - I0_OFFSET/4));
@@ -74,7 +74,7 @@ static uint32_t *os_arch_sched_wait(uint32_t *ctx) {
  * Yield function handler.
  * Release the processor and give another task the opportunity to run.
  */
-static uint32_t *os_arch_sched_yield(uint32_t *ctx) {
+uint32_t *os_arch_sched_yield(uint32_t *ctx) {
   os_task_id_t current_task_id;
   os_task_id_t new_task_id;
 
@@ -100,7 +100,7 @@ static uint32_t *os_arch_sched_yield(uint32_t *ctx) {
  * Mailbox receive function handler.
  * We get the arguments from the stack and we call the os_mbx_receive function.
  */
-static uint32_t *os_arch_mbx_receive(uint32_t *ctx) {
+uint32_t *os_arch_mbx_receive(uint32_t *ctx) {
   os_status_t status;
   os_mbx_entry_t *entry =
       (os_mbx_entry_t *)os_task_ro[os_sched_get_current_task_id()]
@@ -125,7 +125,7 @@ static uint32_t *os_arch_mbx_receive(uint32_t *ctx) {
  * Mailbox send function handler.
  * We get the arguments from the stack and we call the os_mbx_send function.
  */
-static uint32_t *os_arch_mbx_send(uint32_t *ctx) {
+uint32_t *os_arch_mbx_send(uint32_t *ctx) {
   os_status_t status;
   os_mbx_entry_t *entry =
       (os_mbx_entry_t *)os_task_ro[os_sched_get_current_task_id()]
@@ -150,7 +150,7 @@ static uint32_t *os_arch_mbx_send(uint32_t *ctx) {
  * Exit function handler.
  * Handle the case when a task ends.
  */
-static uint32_t *os_arch_sched_exit(uint32_t *ctx) {
+uint32_t *os_arch_sched_exit(uint32_t *ctx) {
   os_task_id_t current_task_id;
   os_task_id_t new_task_id;
 
@@ -168,54 +168,6 @@ static uint32_t *os_arch_sched_exit(uint32_t *ctx) {
   } 
 
   return ctx;
-}
-
-/**
- * Function called by interrupt pre-handler.
- * Call the correct handler for the given trap number.
- * @param trap_nb The number of the current trap. (cf SPARC V8 Manual, page 76)
- * @param stack_pointer Adress of the interrupted stack.
- */
-uint32_t *os_arch_trap_handler(uint32_t *pc, uint32_t *npc, uint32_t psr,
-                               uint32_t trap_nb, uint32_t restore_counter,
-                               uint32_t *stack_pointer) {
-  (void)restore_counter;
-  (void)pc;
-  (void)npc;
-  (void)psr;
-
-  switch (trap_nb) {
-  case (SPARC_TRAP_SYSCALL_BASE + 0):
-    return os_arch_sched_wait(stack_pointer);
-    break;
-  case (SPARC_TRAP_SYSCALL_BASE + 1):
-    return os_arch_sched_yield(stack_pointer);
-    break;
-  case (SPARC_TRAP_SYSCALL_BASE + 2):
-    return os_arch_mbx_send(stack_pointer);
-    break;
-  case (SPARC_TRAP_SYSCALL_BASE + 3):
-    return os_arch_mbx_receive(stack_pointer);
-    break;
-  case (SPARC_TRAP_SYSCALL_BASE + 4):
-    return os_arch_sched_exit(stack_pointer);
-    break;
-  default:
-    printf("[KERNEL] [ERROR] Unhandled trap: 0x%x %%PSR=%x %%PC=%p %%nPC=%p "
-           "%%sp=0x%p\n",
-           trap_nb, psr, pc, npc, stack_pointer);
-    printf("%%psr : impl:0x%x ver:%x nzvc:%u%u%u%u EC:%u EF:%u PIL:0x%x S:%u "
-           "PS:%u ET:%u CWP:%u\n\r",
-           (psr >> 28) & 0xF, (psr >> 24) & 0xF, (psr >> 23) & 0x1,
-           (psr >> 22) & 0x1c, (psr >> 21) & 0x1, (psr >> 20) & 0x1,
-           (psr >> 23) & 0x1, (psr >> 12) & 0x1, (psr >> 8) & 0xF,
-           (psr >> 7) & 0x1, (psr >> 6) & 0x1, (psr >> 5) & 0x1, psr & 0xF);
-    // infinite loop
-    while (1) {
-      os_arch_idle();
-    }
-    break;
-  }
 }
 
 /**
